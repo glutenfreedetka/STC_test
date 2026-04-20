cd0c87d433b56b4b6d8c748b72abdd219fd7f8a7
glutenfreedetka <gleb.kuzmin.01.07@gmail.com>
2026-04-20 21:57:50 +0300

    init

---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+    const auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    std::cout << "version: " << __cplusplus << std::endl;
+
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
